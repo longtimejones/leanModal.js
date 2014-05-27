11647bbad8c2fd18b9d3e2532f7d5817f5418809
Tim Jong Olesen <longtimejones@gmail.com>
2014-05-27 12:46:53 +0200

    Adding support for content loading via AJAX request

--- jquery.leanModal.js
+++ jquery.leanModal.js
@@ -1,73 +1,118 @@
-(function($){
- 
-    $.fn.extend({ 
-         
-        leanModal: function(options) {
- 
+(function ($) {
+
+    $.fn.extend({
+
+        leanModal: function (options) {
+
             var defaults = {
-                top: 100,
-                overlay: 0.5,
-                closeButton: null
+                top: null,
+                opacity: 0.5,
+                overlayClassName: 'lean_overlay',
+                closeButton: null,
+                type: null
             }
-            
-            var overlay = $("<div id='lean_overlay'></div>");
-            
-            $("body").append(overlay);
-                 
-            options =  $.extend(defaults, options);
- 
-            return this.each(function() {
-            
-                var o = options;
-               
-                $(this).click(function(e) {
-              
-              	var modal_id = $(this).attr("href");
 
-				$("#lean_overlay").click(function() { 
-                     close_modal(modal_id);                    
+            var o = $.extend(defaults, options);
+
+            var overlay = $('<div id="' + o.overlayClassName + '"></div>');
+
+            $('body').append(overlay);
+
+            return this.each(function () {
+
+                $(this).click(function (e) {
+
+                    e.preventDefault();
+
+                    var modalTarget = $(this).attr('href');
+
+                    if (o.type == 'ajax') {
+
+                        $.get(modalTarget, function(data) {
+
+                            modalTarget = Math.random().toString().substr(2);
+
+                            $('<div id="' + modalTarget + '"></div>').html(data).appendTo('body');
+
+                            modalTarget = '#' + modalTarget;
+
+                            openModal(modalTarget);
+
+                        });
+
+                    } else {
+
+                        openModal(modalTarget);
+
+                    }
+
                 });
-                
-                $(o.closeButton).click(function() { 
-                     close_modal(modal_id);                    
-                });
-                         	
-              	var modal_height = $(modal_id).outerHeight();
-        	  	var modal_width = $(modal_id).outerWidth();
 
-        		$('#lean_overlay').css({ 'display' : 'block', opacity : 0 });
-
-        		$('#lean_overlay').fadeTo(200,o.overlay);
-
-        		$(modal_id).css({ 
-        		
-        			'display' : 'block',
-        			'position' : 'fixed',
-        			'opacity' : 0,
-        			'z-index': 11000,
-        			'left' : 50 + '%',
-        			'margin-left' : -(modal_width/2) + "px",
-        			'top' : o.top + "px"
-        		
-        		});
-
-        		$(modal_id).fadeTo(200,1);
-
-                e.preventDefault();
-                		
-              	});
-             
             });
 
-			function close_modal(modal_id){
+            function openModal(modalTarget) {
 
-        		$("#lean_overlay").fadeOut(200);
+                $(overlay).click(function () {
+                    closeModal(modalTarget);
+                });
 
-        		$(modal_id).css({ 'display' : 'none' });
-			
-			}
-    
+                $(o.closeButton).click(function () {
+                    closeModal(modalTarget);
+                });
+
+                $(modalTarget).css({
+                    'float': 'left',
+                    'position': 'fixed',
+                    'z-index': 9999
+                });
+
+                var modalHeight = $(modalTarget).outerHeight();
+                var modalWidth  = $(modalTarget).outerWidth();
+                
+                var marginTop   = '0px';
+                var posTop      = o.top + 'px';
+
+                if (o.top === null) {
+
+                    marginTop = -(modalHeight / 2) + 'px';
+                    posTop    = '50%';
+
+                }
+
+                $(overlay).css({
+                    'display': 'block',
+                    opacity: 0
+                })
+                .fadeTo(200, o.opacity);
+
+                $(modalTarget).css({
+                    'display': 'block',
+                    'left':  '50%',
+                    'margin-left': -(modalWidth / 2) + 'px',
+                    'margin-top': marginTop,
+                    'opacity': 0,
+                    'top': posTop
+                })
+                .fadeTo(200, 1);
+
+            }
+
+            function closeModal(modalTarget) {
+
+                overlay.fadeOut(200);
+
+                $(modalTarget).css('display', 'none');
+
+                if (o.type == 'ajax') {
+
+                    $(modalTarget).remove();
+
+                }
+
+            }
+
         }
+
     });
-     
+
 })(jQuery);
